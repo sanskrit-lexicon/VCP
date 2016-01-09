193a5513e3debbd1f1235a5354531f6cfadf0d57
drdhaval2785 <drdhaval2785@gmail.com>
2016-01-09 16:54:47 +0530

    first trial towards generating alternate headwords

--- alternateheadword/hw1_dhaval.py
+++ alternateheadword/hw1_dhaval.py
@@ -1,5 +1,5 @@
 """hw1.py  ejf Oct 1,2013 for vcp
- Usage - python hw1_dhaval.py vcphw0.txt vcphw1_dhaval.txt vcphw1_dhaval_note.txt 
+ Usage - python hw1_dhaval.py vcphw0.txt vcphw1_dhaval.txt vcphw1_dhaval_note.txt
  Read  hw0.txt, whose lines were created with:
  out = "%s:%s:%s,%s" %(page,hw,l1,l2)
 
@@ -32,6 +32,105 @@ hw_override = {'UrdDa(rdDva)':'UrdDva',
   'purU(ru)ravasa':'puru(rU)ravasa',
   'pUrRakU(ku)wa':'pUrRaku(kU)wa'
   }
+def bracketseparator(filein):
+ fin = codecs.open(filein,'r','utf-8')
+ fout = codecs.open('bracketwords.txt','w','utf-8')
+ data = fin.readlines()
+ counter = 0
+ for hw in data:
+  hw = hw.strip()
+  if re.search('\(.*\)',hw):
+   fout.write(hw+"\n")
+   counter += 1
+ print counter, "words have brackets and are put in bracketwords.txt"
+ fout.close()
+def separator(hw):
+ hw = hw.strip()
+ page,hw,lines = hw.split(':')
+ l1,l2 = lines.split(',')
+ return (page,hw,l1,l2)
+def glueto(hw):
+ global counter1, counter2
+ hw = hw.replace(' ','')
+ hw = hw.replace('*','')
+ m = re.search('(.*)[(](.+)[)](.*)',hw)
+ pre, mid, post = m.group(1), m.group(2), m.group(3)
+ if re.search('abBr.{1}[(]Br.{1}[)]',hw): # abBra(Bra)puzpa, abBro(Bro)tTa
+  #print hw, "6"
+  out = "a"+mid+post+":6"
+ elif re.search('UrdDa[(]rdDva[)]',hw): # abBra(Bra)puzpa, abBro(Bro)tTa
+  #print hw, "7"
+  out = "UrdDva"+post+":7"
+ elif pre[-len(mid):].startswith(mid[0]) and len(pre)>=len(mid) and not pre[-len(mid):]==mid:
+  #print hw, "1"
+  out = pre[:-len(mid)]+mid+post+":1"
+ elif pre[-len(mid):].endswith(mid[-1]) and len(pre)>=len(mid) and not pre[-len(mid):]==mid:
+  #print hw, "2"
+  out = pre[:-len(mid)]+mid+post+":2"
+ elif post[:len(mid)].startswith(mid[0]) and len(post)>=len(mid) and not post[:len(mid)]==mid:
+  #print hw, "3"
+  out = pre+mid+post[len(mid):]+":3"
+ elif post[:len(mid)].endswith(mid[-1]) and len(post)>=len(mid) and not post[:len(mid)]==mid:
+  #print hw, "4"
+  out = pre+mid+post[len(mid):]+":4"
+ elif re.search('.{1}[(].{1}[)]',hw): # a(A)nEpuRa
+  #print hw, "5"
+  out = mid+post+":5"
+ else:
+  out = pre+post+":404"
+ return out
+def bracketanalyser(filein):
+ fin = codecs.open(filein,'r','utf-8')
+ midbracketfile = codecs.open('midbracket.txt','w','utf-8') # There is no entry which has '(' at the starting.
+ endbracketfile = codecs.open('endbracket.txt','w','utf-8')
+ data = fin.readlines()
+ fin.close()
+ incount = len(data)
+ counter1 = 0
+ counter2 = 0
+ for hw in data:
+  hw = hw.strip()
+  if re.search('[:].+[(].+[)].+[:]',hw):
+   (page,headword,l1,l2) = separator(hw)
+   out = page+":"+headword+":"+l1+","+l2+":"+glueto(headword)
+   midbracketfile.write(out+"\n")
+   counter1 += 1
+  elif re.search('[)][:]',hw):
+   endbracketfile.write(hw+"\n")
+   counter2 += 1
+ print counter1, "entries written to midbracket.txt"
+ print counter2, "entries written to endbracket.txt"
+ print counter1+counter2, "/", incount, "entries written to respective files"
+ midbracketfile.close()
+ endbracketfile.close()
+ fin = codecs.open('midbracket.txt','r','utf-8')
+ data = fin.readlines()
+ fin.close()
+ hwlistfile = codecs.open('../data/hw1.txt','r','utf-8')
+ validatedfile = codecs.open('validated.txt','w','utf-8')
+ nonvalidatedfile = codecs.open('nonvalidated.txt','w','utf-8')
+ basehwlist = [member.strip() for member in hwlistfile.readlines()]
+ print "validating the suggested items against the list of headwords in ../data/hw1.txt"
+ validatecounter = 0
+ nonvalidatecounter = 0
+ for line in data:
+  line=line.strip()
+  (page,hw,l1l2,suggest,code) = line.split(':')
+  if suggest in basehwlist and not code=="404":
+   validatedfile.write(line+"\n")
+   validatecounter += 1
+  elif re.search('v.*[(].*b.*[)]',hw) and suggest.replace('b','v') in basehwlist: # ava(ba)hitTA, pUrva(rba)karmman etc specific for bengal dictionaries
+   validatedfile.write(line+"\n")
+   validatecounter += 1
+  elif re.search('b.*[(].*v.*[)]',hw) and suggest.replace('v','b') in basehwlist: # karbU(rvU)ra etc specific for bengal dictionaries
+   validatedfile.write(line+"\n")
+   validatecounter += 1
+  else:
+   nonvalidatedfile.write(line+"\n")
+   nonvalidatecounter += 1
+ print validatecounter, "validated entries"
+ print nonvalidatecounter, "non validated entries"
+   
 def hw_normalize(hw):
  hw0 = hw
  if hw in hw_override:
@@ -85,4 +184,6 @@ if __name__=="__main__":
  filein=sys.argv[1] 
  fileout =sys.argv[2] 
  filenote =sys.argv[3]
- hw1(filein,fileout,filenote)
+ #hw1(filein,fileout,filenote)
+ bracketseparator(filein)
+ bracketanalyser('bracketwords.txt')
