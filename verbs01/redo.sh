2bf21645f5e65e3bf64cca1320224113380c01ac
funderburkjim <funderburkjim@gmail.com>
2020-03-02 20:23:56 -0500

    add verb krama

--- verbs01/redo.sh
+++ verbs01/redo.sh
@@ -2,8 +2,11 @@ echo "remake mwverbs"
 python mwverb.py mw ../../mw/mw.txt mwverbs.txt
 echo "remake mwverbs1"
 python mwverbs1.py mwverbs.txt mwverbs1.txt
+echo "remake temp_vcp.txt"
+python updateByLine.py ../vcp.txt preverb_manualByLine.txt temp_vcp.txt
+
 echo "remake vcp_verb_filter"
-python vcp_verb_filter.py ../vcp.txt vcp_verb_filter_exclude.txt vcp_verb_filter_include.txt vcp_verb_filter.txt
+python vcp_verb_filter.py temp_vcp.txt vcp_verb_filter_exclude.txt vcp_verb_filter_include.txt vcp_verb_filter.txt
 echo "remake vcp_verb_filter_map"
 python vcp_verb_filter_map.py vcp_verb_filter.txt mwverbs1.txt vcp_verb_filter_map.txt
 echo "remake vcp_preverb1.txt"
@@ -12,3 +15,7 @@ echo "remake vcp_verb1.txt"
 python verb1.py slp1 temp_vcp.txt vcp_preverb1.txt vcp_verb1.txt
 echo "remake vcp_verb1_deva.txt"
 python verb1.py deva temp_vcp.txt vcp_preverb1.txt vcp_verb1_deva.txt
+
+echo "REDO.SH done"
+echo "now manually copy to sanskrit-lexicon/vcp/verbs01/ using"
+echo "sh redo_copy.sh"
