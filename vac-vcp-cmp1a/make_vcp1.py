62d59140e851863e6d220949ffa61aca671b6658
funderburkjim <funderburkjim@gmail.com>
2021-03-18 15:58:19 -0400

    tidy up initial commit of vcp2

--- vac-vcp-cmp1a/make_vcp1.py
+++ vac-vcp-cmp1a/make_vcp1.py
@@ -2,12 +2,15 @@
 import sys,re,codecs
 
 if __name__ == "__main__":
- filein = sys.argv[1]
- fileout = sys.argv[2]
+ filein = sys.argv[1] # vcp.txt
+ fileout = sys.argv[2] # vcp1a.txt
+ fileout1 = sys.argv[3]  # map file
  f = codecs.open(filein,"r","utf-8")
  fout = codecs.open(fileout,"w","utf-8")
+ fmap = codecs.open(fileout1,"w","utf-8")
  n = 0
  outlines = []
+ outmap = []
  for iline,line in enumerate(f):
   line = line.rstrip('\r\n')
   if line.startswith(('<L>','<LEND>')):
@@ -23,12 +26,19 @@ if __name__ == "__main__":
   if iline == 1:
    newline = line + page
    outlines.append(newline)
+   n = n + 1
+   outmap.append('%s %s' %(n,iline+1))
    continue
   newline = re.sub(r'^(.*?)¦',r'<HI>{@\1@}¦',line)
   outlines.append(newline)
+  n = n + 1
+  outmap.append('%s %s' %(n,iline+1))
  print(len(outlines))
  f.close()
  for line in outlines:
   fout.write(line+'\n')
  fout.close()
+ for line in outmap:
+  fmap.write(line+'\n')
+ fmap.close()
  
---
+++ vac-vcp-cmp1a/redo_vcp2.sh
@@ -0,0 +1,4 @@
+#python make_vcp1a.py temp_vcp.txt temp_vcp1a.txt temp_vcp2a_map.txt
+#python make_vcp2a.py vac2.txt temp_vcp1a.txt vcp2a.txt
+python make_vcp1.py vcp.txt temp_vcp1.txt vcp2_map.txt
+python make_vcp2.py vac2.txt temp_vcp1.txt vcp2.txt
